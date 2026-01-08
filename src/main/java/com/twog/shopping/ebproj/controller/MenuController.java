a8f7528b5d13b02de074853d59070d5e021a5b7d
hyunn9799 <underwater0518@gmail.com>
2026-01-08 13:39:55 +0900

    first feature/test

---
+++ src/main/java/com/twog/shopping/ebproj/controller/MenuController.java
@@ -0,0 +1,13 @@
+package com.twog.shopping.ebproj.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenuController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "I'm alive!";
+    }
+}
