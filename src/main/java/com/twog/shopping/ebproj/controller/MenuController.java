f70e55bbfe348a463c21db293af7d400856b9630
hyunn9799 <underwater0518@gmail.com>
2026-01-09 09:42:26 +0900

    jin

--- src/main/java/com/twog/shopping/ebproj/controller/MenuController.java
+++ src/main/java/com/twog/shopping/ebproj/controller/MenuController.java
@@ -1,13 +1,25 @@
 package com.twog.shopping.ebproj.controller;
 
+import com.twog.shopping.ebproj.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MenuController {
 
+    private final MenuService menuService;
+
+    @Autowired
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
     @GetMapping("/health")
     public String healthCheck() {
         return "I'm alive!";
     }
+
+
+
 }
--- src/main/java/com/twog/shopping/ebproj/service/MenuService.java
+++ src/main/java/com/twog/shopping/ebproj/service/MenuService.java
@@ -1,5 +1,18 @@
 package com.twog.shopping.ebproj.service;
 
+import com.twog.shopping.ebproj.repository.MenuRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MenuService {
     // service
+
+    private final MenuRepository menuRepository;
+    private final ModelMapper modelMapper;
+
+    public MenuService(MenuRepository menuRepository, ModelMapper modelMapper) {
+        this.menuRepository = menuRepository;
+        this.modelMapper = modelMapper;
+    }
 }
