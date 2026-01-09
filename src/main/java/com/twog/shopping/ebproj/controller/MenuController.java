153f90e868735efa60a3d2183667236d713d6264
hyunn9799 <underwater0518@gmail.com>
2026-01-09 10:07:46 +0900

    jin

--- src/main/java/com/twog/shopping/ebproj/controller/MenuController.java
+++ src/main/java/com/twog/shopping/ebproj/controller/MenuController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MenuController {
-
+//controller
     private final MenuService menuService;
 
     @Autowired
